b1107fb17fdeafe43107a96dcf588aa1123248c5
aldy1823 <rinaldy.desri@gmail.com>
2026-04-14 22:49:21 +0700

    Prettier Adjusment

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,46 +1,44 @@
-import { OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import { useMediaQuery } from 'react-responsive'
-import { Room } from './Room'
-import HeroLights from './HeroLights'
-import Particles from './Particles'
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import { Room } from "./Room";
+import HeroLights from "./HeroLights";
+import Particles from "./Particles";
 
 const HeroExperience = () => {
-    const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
-    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
-    
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
   return (
-   <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+      <HeroLights />
 
-    <HeroLights />
+      <Particles count={100} />
 
-    <Particles count={100} />
+      <OrbitControls
+        enablePan={false}
+        enableZoom={!isTablet}
+        maxDistance={20}
+        minDistance={5}
+        minPolarAngle={Math.PI / 5}
+        maxPolarAngle={Math.PI / 2}
+      />
 
-    <OrbitControls 
-    enablePan={false}
-    enableZoom={!isTablet}
-    maxDistance={20}
-    minDistance={5}
-    minPolarAngle={Math.PI / 5}
-    maxPolarAngle={Math.PI / 2}
-    />
+      <group
+        scale={isMobile ? 0.7 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
+        <Room />
+      </group>
 
-    <group
-    scale={isMobile ? 0.7 : 1}
-    position={[0, -3.5, 0]} 
-    rotation={[0, -Math.PI/4, 0]}
-    >
-    <Room />
-    </group>
-
-
-    {/* <mesh>
+      {/* <mesh>
         <boxGeometry args={[1, 1, 1]}  />
         <meshStandardMaterial color={teal} />
     </mesh> */}
-   </Canvas>
-  )
-}
+    </Canvas>
+  );
+};
 
-export default HeroExperience
+export default HeroExperience;
